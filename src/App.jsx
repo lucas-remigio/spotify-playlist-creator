f57a2c9973f99ea8861373abb6441f94afd7f433
Lucas Remigio <2211034@my.ipleiria.pt>
2024-05-21 00:59:37 +0100

    feature: show user's playlists

--- src/App.jsx
+++ src/App.jsx
@@ -1,34 +1,114 @@
-import { useState } from 'react'
-import reactLogo from './assets/react.svg'
-import viteLogo from '/vite.svg'
 import './App.css'
+import { useEffect, useState, useCallback } from 'react'
+import axios from 'axios'
 
 function App() {
-  const [count, setCount] = useState(0)
+  const CLIENT_ID = '455e44d248cd4522a12e519b7a75ea89'
+  const REDIRECT_URI = 'http://localhost:5173'
+  const AUTH_ENDPOINT = 'https://accounts.spotify.com/authorize'
+  const RESPONSE_TYPE = 'token'
+
+  const [token, setToken] = useState('')
+  const [playlists, setPlaylists] = useState([])
+  const [selectedPlaylistId, setSelectedPlaylistId] = useState(null)
+
+  const GetUserPlaylists = useCallback(async () => {
+    try {
+      const response = await axios.get('https://api.spotify.com/v1/me/playlists', {
+        headers: {
+          Authorization: `Bearer ${token}`,
+        },
+      })
+      setPlaylists(response.data.items)
+      console.log(response.data.items)
+    } catch (error) {
+      console.error('Error fetching playlists:', error)
+    }
+  }, [token])
+
+  const getProfile = useCallback(async () => {
+    try {
+      const profile = await axios.get('https://api.spotify.com/v1/me', {
+        headers: {
+          Authorization: `Bearer ${token}`,
+        },
+      })
+      console.log(profile.data)
+    } catch (error) {
+      console.error('Error fetching profile:', error)
+    }
+  }, [token])
+
+  const handlePlaylistSelect = (playlistId) => {
+    setSelectedPlaylistId(playlistId)
+  }
+
+  useEffect(() => {
+    const hash = window.location.hash
+    let storedToken = window.localStorage.getItem('token')
+
+    if (!storedToken && hash) {
+      storedToken = hash
+        .substring(1)
+        .split('&')
+        .find((elem) => elem.startsWith('access_token'))
+        .split('=')[1]
+
+      window.location.hash = ''
+      window.localStorage.setItem('token', storedToken)
+    }
+
+    setToken(storedToken)
+  }, [])
+
+  useEffect(() => {
+    if (token) {
+      getProfile()
+      GetUserPlaylists()
+    }
+  }, [token, getProfile, GetUserPlaylists])
+
+  const logout = () => {
+    setToken('')
+    window.localStorage.removeItem('token')
+  }
+
+  const login = () => {
+    const loginUrl = `${AUTH_ENDPOINT}?client_id=${CLIENT_ID}&redirect_uri=${REDIRECT_URI}&response_type=${RESPONSE_TYPE}`
+    window.location.href = loginUrl
+  }
 
   return (
-    <>
-      <div>
-        <a href="https://vitejs.dev" target="_blank">
-          <img src={viteLogo} className="logo" alt="Vite logo" />
-        </a>
-        <a href="https://react.dev" target="_blank">
-          <img src={reactLogo} className="logo react" alt="React logo" />
-        </a>
-      </div>
-      <h1>Vite + React</h1>
-      <div className="card">
-        <button onClick={() => setCount((count) => count + 1)}>
-          count is {count}
-        </button>
-        <p>
-          Edit <code>src/App.jsx</code> and save to test HMR
-        </p>
-      </div>
-      <p className="read-the-docs">
-        Click on the Vite and React logos to learn more
-      </p>
-    </>
+    <div className="App">
+      <header className="App-header">
+        <h1>Spotify React</h1>
+        {!token ? (
+          <button onClick={login}>Login to Spotify</button>
+        ) : (
+          <>
+            <button onClick={logout}>Logout</button>
+            <div>
+              <h2>My Playlists:</h2>
+              <ul className="playlist-container">
+                {playlists.map((playlist) => (
+                  <li key={playlist.id} className="playlist-card">
+                    <div className="playlist-content">
+                      <img
+                        src={playlist.images[0].url}
+                        alt={playlist.name}
+                        className="playlist-image"
+                      />
+                      <h3 className="playlist-name">{playlist.name}</h3>
+                      <button onClick={() => handlePlaylistSelect(playlist.id)}>Select</button>
+                    </div>
+                  </li>
+                ))}
+              </ul>
+            </div>
+          </>
+        )}
+      </header>
+    </div>
   )
 }
 
